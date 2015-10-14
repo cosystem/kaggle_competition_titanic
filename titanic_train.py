eeca0fdf58d87ac8137e282b84d452777099f06e
cosystem <jougraw@gmail.com>
2015-10-14 13:33:16 +0200

    The first version of python script for data clean and basic statistics

---
+++ titanic_train.py
@@ -0,0 +1,39 @@
+import os
+import pandas
+# read data file
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(__dir__, "train.csv")
+titanic = pandas.read_csv(filepath)
+# Print the first 5 rows of the dataframe.
+print(titanic.head(10))
+
+# Replace missing value in "Age" with the median value
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+print(titanic.describe())
+
+
+# function to generate tutple used in label assignment
+def packgen(inString):
+    uniquelist = list(titanic[inString].unique())
+    numberlist = list(range(len(uniquelist)))
+    package = (uniquelist, numberlist)
+    return package
+# function to assign labels
+def assign(inTuple, remainnumber, assignlabel):
+    if len(inTuple[0]) > remainnumber:
+        label = inTuple[0].pop(0)
+        number = inTuple[1].pop(0)
+        titanic.loc[titanic[assignlabel] == label, assignlabel] = number
+        assign(inTuple, remainnumber, assignlabel)
+
+# Find all the unique genders, and assign label
+print(titanic["Sex"].unique())
+tupleSex=packgen("Sex")
+assign(tupleSex, 0, "Sex")
+print(titanic["Sex"].unique())
+
+# Find all the unique Embark, and assign label
+print(titanic["Embarked"].unique())
+tupleEmbar=packgen("Embarked")
+assign(tupleEmbar, 1, "Embarked")
+print(titanic["Embarked"].unique())
